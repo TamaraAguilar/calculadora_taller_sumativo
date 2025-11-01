17a9e33c8e9f9403b1ecd9c1680a2bdfa1433836
taguilar <taguilar@entel.cl>
2025-11-01 17:33:31 -0300

    feat: añadir función para sumar

---
+++ operaciones/suma.py
@@ -0,0 +1,12 @@
+def suma (a, b):
+    """
+    Esta función suma dos números y retorna el resultado.
+    
+    Args:
+        a (int/float): Primer número
+        b (int/float): Segundo número
+    
+    Returns:
+        int/float: La suma de a y b
+    """
+    return a+b
