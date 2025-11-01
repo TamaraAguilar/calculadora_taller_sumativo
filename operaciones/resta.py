775af93f5f33fe2a483d436a8db2bd7b5b26c6b8
taguilar <taguilar@entel.cl>
2025-11-01 17:34:24 -0300

    feat: añadir función para restar

---
+++ operaciones/resta.py
@@ -0,0 +1,12 @@
+def resta (a, b):
+    """
+    Esta función resta dos números y retorna el resultado.
+    
+    Args:
+        a (int/float): Primer número
+        b (int/float): Segundo número
+    
+    Returns:
+        int/float: La resta de a y b
+    """
+    return a-b
