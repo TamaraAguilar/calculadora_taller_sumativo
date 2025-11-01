02939bc8375a357afe5471fc2e102e996cf893a2
taguilar <taguilar@entel.cl>
2025-11-01 17:45:56 -0300

    feat: añadir función para obtener la potencia de dos números

---
+++ operaciones/exponente.py
@@ -0,0 +1,12 @@
+def exponente (a, b):
+    """
+    Esta función eleva una base a un exponente
+    
+    Args:
+        a (int/float): base
+        b (int/float): exponente
+    
+    Returns:
+        int/float: El resultado de a elevado a la potencia b
+    """
+    return a**b
