b503641bde450eb4190e66145b29a8cb1d8e03d4
taguilar <taguilar@entel.cl>
2025-11-01 17:36:10 -0300

    feat: añadir función para multiplicar

---
+++ operaciones/division.py
@@ -0,0 +1,12 @@
+def division (a, b):
+    """
+    Esta función divide dos números y retorna el resultado.
+    
+    Args:
+        a (int/float): Primer número
+        b (int/float): Segundo número
+    
+    Returns:
+        int/float: La división de a y b
+    """
+    return a/b
