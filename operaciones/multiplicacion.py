b1a3c167f43d5bd066a60b2af8e4a9dc2e8c6fc5
taguilar <taguilar@entel.cl>
2025-11-01 17:34:54 -0300

    feat: añadir función para multiplicar

---
+++ operaciones/multiplicacion.py
@@ -0,0 +1,12 @@
+def multiplicar (a, b):
+    """
+    Esta función multiplica dos números y retorna el resultado.
+    
+    Args:
+        a (int/float): Primer número
+        b (int/float): Segundo número
+    
+    Returns:
+        int/float: La multiplicación de a y b
+    """
+    return a*b
