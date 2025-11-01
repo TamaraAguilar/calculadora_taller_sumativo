a5926f90a83521d2560192c927231abafc6c0358
taguilar <taguilar@entel.cl>
2025-11-01 17:42:16 -0300

    feat: añadir función para obtener el módulo

---
+++ operaciones/modulo.py
@@ -0,0 +1,12 @@
+def modulo (a, b):
+    """
+    Esta función devuelve el resto de una división.
+    
+    Args:
+        a (int/float): Primer número
+        b (int/float): Segundo número
+    
+    Returns:
+        int/float: El resto de a y b
+    """
+    return a%b
